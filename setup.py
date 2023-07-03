125edec8d50f2008e1f6b6bcd7adcbcdf7e4da2d
vince <vince.it@icloud.com>
2023-07-03 04:08:33 -0700

    Update setup.py
    Signed-off-by: .vince <vince.it@icloud.com>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.11.4',
+        'Programming Language :: Python :: 3.11.4'
 
 
 
