3f7f5fce86dcee93628dd6a8454eae8cbabad77e
vince <vince.it@icloud.com>
2023-07-22 08:41:47 +0000

    added ospf class to dynamic_routing.py

--- ciscopykit/routing/dynamic_routing.py
+++ ciscopykit/routing/dynamic_routing.py
@@ -30,20 +30,84 @@ Example:
     print(rip_config)
 
 """
-
+import ipaddress
 
 class DynamicRoutingProtocol:
+    """
+    Base class for dynamic routing protocols.
+
+    This class serves as a template for implementing dynamic routing protocols in CiscoPyKit.
+    Subclasses must implement the 'configure' and 'generate_config' methods to configure the
+    respective routing protocols on the device and generate the configuration.
+
+    Attributes:
+        networks (list): A list of network addresses to be advertised.
+
+    Methods:
+        configure(): Abstract method to configure the dynamic routing protocol on the device.
+            Subclasses must implement this method.
+        
+        generate_config(): Abstract method to generate the configuration for the dynamic routing
+            protocol. Subclasses must implement this method.
+
+    Raises:
+        NotImplementedError: Raised when 'configure' and 'generate_config' methods are not implemented
+            by the subclass.
+    """
+
     def __init__(self, networks):
+        """
+        Initialize a dynamic routing protocol.
+
+        Args:
+            networks (list): A list of network addresses to be advertised.
+        """
         self.networks = networks
 
     def configure(self):
+        """
+        Abstract method to configure the dynamic routing protocol on the device.
+
+        Subclasses must implement this method.
+
+        Raises:
+            NotImplementedError: Raised when the method is not implemented by the subclass.
+        """
         raise NotImplementedError("Subclasses must implement the 'configure' method.")
 
     def generate_config(self):
+        """
+        Abstract method to generate the configuration for the dynamic routing protocol.
+
+        Subclasses must implement this method.
+
+        Raises:
+            NotImplementedError: Raised when the method is not implemented by the subclass.
+        """
         raise NotImplementedError("Subclasses must implement the 'generate_config' method.")
 
 class RIP(DynamicRoutingProtocol):
+    """
+    Class representing the RIP (Routing Information Protocol) routing protocol.
+
+    RIP is a dynamic routing protocol that is widely used in small to medium-sized networks.
+    It is a distance-vector protocol and uses hop count as the metric to calculate the best
+    path to a destination network.
+
+    Attributes:
+        version (int): The version of RIP to use (1 or 2).
+        networks (list): A list of networks to advertise with RIP.
+        no_auto_summary (bool, optional): Whether to disable auto-summary (default: False).
+        passive_interfaces (list, optional): A list of interfaces to be set as passive (default: None).
     
+    Methods:
+        configure(): Configures the RIP routing protocol on the device.
+        generate_config(): Generates the configuration for the RIP routing protocol.
+
+    Raises:
+        ValueError: If the version is not 1 or 2.
+    """
+
     def __init__(self, version, networks, no_auto_summary=False, passive_interfaces=None):
         """
         Initialize a RIP routing protocol.
@@ -97,22 +161,7 @@ class RIP(DynamicRoutingProtocol):
         """
         # Implementation to generate RIP configuration
         return self.configure()
-
-
-class OSPF(DynamicRoutingProtocol):
-    def __init__(self, process_id, network, area):
-        super().__init__(network)
-        self.process_id = process_id
-        self.area = area
-
-    def configure(self):
-        # Implementation to configure OSPF routing protocol
-        pass
-
-    def generate_config(self):
-        # Implementation to generate OSPF configuration
-        pass
-
+    
 class EIGRP(DynamicRoutingProtocol):
     def __init__(self, as_number, network):
         super().__init__(network)
@@ -126,4 +175,106 @@ class EIGRP(DynamicRoutingProtocol):
         # Implementation to generate EIGRP configuration
         pass
 
+class OSPF(DynamicRoutingProtocol):
+    """
+    Class representing the OSPF (Open Shortest Path First) routing protocol.
+
+    OSPF is a link-state routing protocol that is widely used in large networks.
+    It uses Dijkstra's algorithm to calculate the shortest path to a destination network.
+
+    Attributes:
+        process_id (int): The OSPF process ID.
+        router_id (str): The OSPF router ID.
+        ospf_options (dict, optional): Dictionary containing OSPF-specific options (default: None).
+
+    Raises:
+        ValueError: If the process ID is not a positive integer.
+        ValueError: If the provided IP addresses are not valid.
+    """
+
+    def __init__(self, process_id, router_id, ospf_options=None):
+        """
+        Initialize an OSPF routing protocol.
+
+        Args:
+            process_id (int): The OSPF process ID.
+            router_id (str): The OSPF router ID.
+            ospf_options (dict, optional): Dictionary containing OSPF-specific options (default: None).
+
+        Raises:
+            ValueError: If the process ID is not a positive integer.
+        """
+        if not isinstance(process_id, int) or process_id <= 0:
+            raise ValueError("Invalid OSPF process ID. The process ID must be a positive integer.")
+
+        self.process_id = process_id
+        self.router_id = router_id
+        self.ospf_options = ospf_options or {}
+        self.networks = self.ospf_options.get('networks', [])
+        self.passive_interfaces = self.ospf_options.get('passive_interfaces', [])
+        self.default_route_interface = self.ospf_options.get('default_route_interface')
+
+    def _validate_ip_addresses(self, ip_addresses):
+        for ip in ip_addresses:
+            try:
+                ipaddress.IPv4Network(ip)
+            except ipaddress.AddressValueError:
+                raise ValueError(f"Invalid IP address: {ip}")
+
+    def _cidr_to_network(self, cidr):
+        ip_net = ipaddress.IPv4Network(cidr)
+        return str(ip_net.network_address)
+
+    def configure(self):
+        """
+        Configure OSPF routing protocol on the device.
+
+        Returns:
+            str: The configuration commands for configuring OSPF.
+        """
+        ospf_config = [f"router ospf {self.process_id}"]
+        ospf_config.append(f"router-id {self.router_id}")
+
+        for network in self.networks:
+            network_address = self._cidr_to_network(network)
+            ospf_config.append(f"network {network_address} 0.0.0.255 area 0")
+
+        for interface in self.passive_interfaces:
+            ospf_config.append(f"passive-interface {interface}")
+
+        if self.default_route_interface:
+            ospf_config.append(f"default-information originate always metric 1 metric-type 1 {self.default_route_interface}")
+
+        return "\n".join(ospf_config)
+    
+    def generate_config(self):
+        return self.configure()
+
+
+# Example usage
+
+networks = ["192.168.0.0", "10.0.0.0"]
+passive_interfaces = ["GigabitEthernet0/1", "GigabitEthernet0/2"]
+
+rip_protocol = RIP(version=2, networks=networks, no_auto_summary=True, passive_interfaces=passive_interfaces)
+rip_config = rip_protocol.generate_config()
+
+print(rip_config)
+
+##################################
+# OSPF                          #
+#################################
+ospf_options = {
+    'networks': ['192.168.1.0/24', '10.0.0.0/16'],
+    'passive_interfaces': ['GigabitEthernet0/1', 'Serial0/0/0'],
+    'default_route': True
+}
+
+ospf_instance = OSPF(process_id=10, router_id='6.6.6.6', ospf_options=ospf_options)
+
+# Generate OSPF configuration
+ospf_config = ospf_instance.generate_config()
+
+# Print the generated configuration
+print(ospf_config)
 
